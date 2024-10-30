e394364086d460516821c98e77b6ac983ad50107
teesupasirirat <teesupasirirat>
2024-10-30 08:49:35 +0000

    update file

--- pipeline.py
+++ pipeline.py
@@ -1,15 +1,25 @@
 # Databricks notebook source
 import pandas as pd
 import math
+import os
+import configparser
 
-input_file = "/Workspace/Users/narumopi@ais.co.th/track_small.csv"
-output_file = "/Workspace/Users/narumopi@ais.co.th/output_small.csv"
+# Get the parent path of the notebook
+notebook_path = dbutils.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+parent_path = os.path.dirname('/Workspace' + notebook_path)
+os.chdir(parent_path)
+
+# read config file
+config = configparser.ConfigParser()
+config.read('./pipeline.conf')
+inputPath = config.get('DEFAULT', 'INPUT_PATH')
+outputPath = config.get('DEFAULT', 'OUTPUT_PATH')
 
 # Extract
-tracks = pd.read_csv(input_file)
+tracks = pd.read_csv(inputPath)
 
 # Transform
-tracks['UnitPrice'] = tracks['UnitPrice'].apply(lambda x:math.ceil(x))
-
+tracks["UnitPrice"] = tracks["UnitPrice"].apply(lambda x: math.ceil(x))
+                             
 # Load
-tracks.to_csv(output_file, index=False)
+tracks.to_csv(outputPath, index=False)
